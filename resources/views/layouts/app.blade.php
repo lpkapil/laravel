67bd393a5d1797869d008509bb5a93ead7ebf4c8
kapil-infobeans <kapil.yadav@infobeans.com>
2017-09-01 15:35:52 +0530

    Minor fixes for layout and global session middleware for error handlers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,7 @@
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
